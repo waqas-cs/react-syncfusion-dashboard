ba82541e593cc53495e7f23e1c4c4b7f93969d3a
waqas-cs <waqastarakai.cs@gmail.com>
2025-03-03 11:50:08 +0400

    Sparkline Component

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -4,7 +4,30 @@ import {
   SparklineTooltip,
 } from "@syncfusion/ej2-react-charts";
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  return <SparklineComponent></SparklineComponent>;
+  return (
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="y"
+      type={data}
+      tooltipSettings={{
+        visible: true,
+        format: "${x} : data ${y}",
+        trackLineSettings: {
+          visible: true,
+        },
+      }}
+    >
+      <Inject services={[SparklineTooltip]} />
+    </SparklineComponent>
+  );
 };
 
 export default SparkLine;
